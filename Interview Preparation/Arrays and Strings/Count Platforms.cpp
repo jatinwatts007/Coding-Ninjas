c406ed3fba4d1983ee6ce5bcda9fcd5714550775
Jatin Watts <51354333+jatinwatts007@users.noreply.github.com>
2021-01-07 23:22:29 +0530

    Create Count Platforms.cpp

---
+++ Interview Preparation/Arrays and Strings/Count Platforms.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+int platformsNeeded(int arr1[], int arr2[], int n) {
+    /*
+     * Don't write main().
+     * Don't read input, it is passed as function argument.
+     * Don't print anything just return integer value.
+     */
+    int count=1;
+    int m,temp;
+    
+    sort(arr1,arr1+n);
+    sort(arr2,arr2+n);
+    int j=0;
+    for(int i=1;i<n;i++)
+    {
+        if(arr2[j]>=arr1[i]){
+            count++;
+        }else{
+            j++;
+        }
+    }
+    
+    return count;
+    
+}
