bb4fae706ed427fcd3689163eab24efd60de3a0f
Jatin Watts <51354333+jatinwatts007@users.noreply.github.com>
2021-03-20 23:33:49 +0530

    Update Print Level wise.cpp

--- Interview Preparation/Trees/Print Level wise.cpp
+++ Interview Preparation/Trees/Print Level wise.cpp
@@ -39,3 +39,74 @@ void printLevelWise(TreeNode<int>* root) {
         q.pop();
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/**********************************************************
+
+	Following is the Binary Tree Node class structure
+
+	template <typename T>
+	class BinaryTreeNode {
+    	public : 
+    	T data;
+    	BinaryTreeNode<T> *left;
+    	BinaryTreeNode<T> *right;
+
+    	BinaryTreeNode(T data) {
+        	this -> data = data;
+        	left = NULL;
+        	right = NULL;
+    	}
+	};
+
+***********************************************************/
+
+void printLevelWise(BinaryTreeNode<int> *root) {
+	// Write your code here
+    if(root==NULL) return;
+    queue<BinaryTreeNode<int>*> q;
+    q.push(root);
+    while(!q.empty())
+    {
+        cout<<q.front()->data<<":";
+        if(q.front()->left!=NULL)
+        {
+            cout<<"L:"<<q.front()->left->data<<',';
+            q.push(q.front()->left);
+        }
+        else{
+            cout<<"L:"<<-1<<',';
+        }
+        if(q.front()->right!=NULL)
+        {
+            cout<<"R:"<<q.front()->right->data;
+            q.push(q.front()->right);
+        }
+        else{
+            cout<<"R:"<<-1;
+        }
+        cout<<endl;
+        q.pop();
+    }
+}
